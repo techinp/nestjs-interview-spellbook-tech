2513c67a1342f6ca5476f5221867b106b1c0c423
techinp <techin.pw@gmail.com>
2023-08-26 02:32:39 +0700

    Add public to root controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Logger } from '@nestjs/common';
+import { Public } from './meta';
 
+@Public()
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
